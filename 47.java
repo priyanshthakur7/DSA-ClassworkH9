15866812366f6a17aa46cb6456efc9069bb8273e
priyanshthakur7 <priyanshkrish2005@gmail.com>
2026-09-24 16:36:15 +0530

    Add Permutations I and II solutions

---
+++ 47.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        Arrays.sort(nums);
+
+        boolean[] used = new boolean[nums.length];
+
+        backtrack(nums, new ArrayList<>(), result, used);
+
+        return result;
+    }
+
+    private void backtrack(int[] nums, List<Integer> current,
+            List<List<Integer>> result, boolean[] used) {
+
+        if (current.size() == nums.length) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (used[i]) {
+                continue;
+            }
+
+            // Skip duplicates
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) {
+                continue;
+            }
+
+            used[i] = true;
+            current.add(nums[i]);
+
+            backtrack(nums, current, result, used);
+
+            current.remove(current.size() - 1);
+            used[i] = false;
+        }
+    }
+}
